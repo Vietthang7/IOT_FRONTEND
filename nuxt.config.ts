ed11b68e67c63f9ec8c1e354616ec86d6f134df6
thangnguyen <nguyenvietthang12092004@gmail.com>
2025-09-12 10:04:38 +0700

    refactor: streamline nuxt.config.ts formatting and structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: [
-    '@unocss/nuxt',
-  ],
+  modules: ["@unocss/nuxt"],
   css: ["@/assets/main.css", "@/assets/tailwind.css"],
-})
+  runtimeConfig: {
+    public: {
+      baseUrl: "http://127.0.0.1:3002/api",
+      adminUrl: "",
+      propertyID: "",
+    },
+  },
+});
